ed57c59e6113b919b75e6593342d716a0fd4361e
DanielRath <danielr4th@gmail.com>
2018-10-29 16:06:36 -0300

    29/10/2019 16h

--- code/pc3_2018_10_29.c
+++ code/pc3_2018_10_29.c
@@ -1,8 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main ()
+int opcao;
+
+
+//to do list:
+//identify pid of process: "gvfsd-gphoto2" and kill;
+//create infinite loop for menu
+
+void iniciando()
 {
-	system ("gphoto2");
+	//teste
+}
+
+void menu()
+{
+	printf("Bem-vindo ao dispositivo Smart\n");
+        printf("1- Tirar foto\n");      
+        printf("Escolha a opção desejada\n");
+        scanf("%d", &opcao);
+        printf ("Você escolheu a opção: %d\n", opcao);
+        switch(opcao)
+        {
+                case 1:
+                        system ("gphoto2 --trigger-capture");
+                        printf("Tirando foto\n");
+                break;
+        }
+}
+
+
+int main (int argc, char *argv[])
+{
+	iniciando();
+	menu();
+	
 	return 0;
 }
