967503c861b1a9bc165b9a0e534d259a9b41c2dc
DanielRath <danielr4th@gmail.com>
2018-11-24 12:42:50 -0200

    update

--- code/pc4.c
+++ code/pc4.c
@@ -30,6 +30,7 @@ void ajustes()
 			switch(opcao)
 			{
 				case 1:
+					gphoto2 --get-config /main/capturesettings/shutterspeed
 					system ("gphoto2 --trigger-capture");
 					printf("\nTirando foto\n");
 					system ("clear");
