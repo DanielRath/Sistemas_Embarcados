217d82e8fdce84b2a22dd0781c6369d02fb8073a
DanielRath <37368908+DanielRath@users.noreply.github.com>
2018-11-24 13:00:39 -0200

    Update pc4.c

--- code/pc4.c
+++ code/pc4.c
@@ -42,7 +42,7 @@ void ajustes()
 					system ("gphoto2 --get-config /main/capturesettings/shutterspeed");
 					printf("\nEscolha a opção desejada\n"); 
 					escolha_opcao();
-					system ("gphoto2 --set-config /main/capturesettings/shutterspeed=%d", opcao);
+					system ("gphoto2 --set-config /main/capturesettings/shutterspeed=2");
 				break;
 				case 2: 
 					//inserir
